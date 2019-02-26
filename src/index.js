387be2f8e38511c86d4c4b87b130b8195886ed2b
ssb000ss <ssb000ss@gmail.com>
2019-02-26 01:50:48 -0600

    task was soluted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let crBase = base;
+    let primePows = [];
+    let prime = 2;
+    let obj = {};
+    let arr = [];
+    while (crBase !== 1) {
+        if (crBase % prime === 0) {
+            crBase /= prime;
+            primePows.push(prime);
+            if (obj[prime] === undefined) obj[prime] = 0;
+            if (obj[prime] !== undefined) obj[prime]++;
+        } else {
+            prime++;
+        }
+    }
+    for (let i = 0; i < primePows.length; i++) {
+        let pow = primePows[i];
+        let exp = primePows[i];
+        let tempResult = 0;
+        while (number > pow) {
+            tempResult += Math.floor(number / pow);
+            pow *= exp;
+        }
+        arr.push(Math.floor(tempResult / obj[exp]));
+    }
+    return Math.min.apply('', arr);
+};
+
